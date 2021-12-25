fe5fd38e03f4b5c4a51098ef7bd09a7a81ba322f
Blergo <beardedowen@gmail.com>
2021-12-25 18:28:44 +0000

    Initialised SWSerial + DHT

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,26 @@
 #include <SimpleModbusSlaveSoftwareSerial.h>
 #include <SimpleDHT.h>
 
+int pinDHT11 = 3;
+SimpleDHT11 dht11;
+
+#define RX            0
+#define TX            1
+#define RS485_EN      2
+#define BAUD_RATE     19200
+#define deviceID      3   
+
+enum  {
+  TEMP,
+  HUM,
+  TOTAL_ERRORS,
+  TOTAL_REGS_SIZE 
+};
+
+unsigned int holdingRegs[TOTAL_REGS_SIZE];
+
+SoftwareSerial mySerial(RX, TX);
+
 void setup() {
   // put your setup code here, to run once:
 }
