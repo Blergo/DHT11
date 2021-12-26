6e891bb591b1515d983fafed9c5bfa71ae6dd275
Blergo <beardedowen@gmail.com>
2021-12-26 19:51:08 +0000

    changed DHT library

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <SimpleModbusSlaveSoftwareSerial.h>
-#include <SimpleDHT.h>
+#include <dht.h>
 
-int pinDHT11 = 3;
-SimpleDHT11 dht11;
+int DHT11_PIN = 2;
+dht DHT;
+byte temperature = 0;
+byte humidity = 0;
 
-#define RX            0
-#define TX            1
+#define RX            1
+#define TX            0
 #define RS485_EN      -1
 #define BAUD_RATE     9600
-#define deviceID      247   
+#define deviceID      1
 
 enum  {
   TEMP,
@@ -24,20 +26,16 @@ unsigned int holdingRegs[TOTAL_REGS_SIZE];
 SoftwareSerial mySerial(RX, TX);
 
 void setup() {
+  mySerial.begin(19200);
   modbus_configure(&mySerial, BAUD_RATE, deviceID, RS485_EN, TOTAL_REGS_SIZE);
 }
 
 void loop() {
-  byte temperature = 0;
-  byte humidity = 0;
-  int err = SimpleDHTErrSuccess;
-  if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    holdingRegs[0] = 0;
-    holdingRegs[1] = 0;
-    return;
-  }
 
-  holdingRegs[0] = (int)temperature;
-  holdingRegs[1] = (int)humidity;
+  DHT.read11(DHT11_PIN);
+
+  holdingRegs[0] = (int)DHT.temperature;
+  holdingRegs[1] = (int)DHT.humidity;
   holdingRegs[TOTAL_ERRORS] = modbus_update(holdingRegs);
+  delay(2000);
 }
