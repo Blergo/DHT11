45b8698913e8c2171724efc464a62f648dc55945
Blergo <beardedowen@gmail.com>
2021-12-26 14:32:45 +0000

    Commit

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ SimpleDHT11 dht11;
 
 #define RX            0
 #define TX            1
-#define RS485_EN      2
-#define BAUD_RATE     19200
+#define RS485_EN      -1
+#define BAUD_RATE     9600
 #define deviceID      247   
 
 enum  {
@@ -24,9 +24,20 @@ unsigned int holdingRegs[TOTAL_REGS_SIZE];
 SoftwareSerial mySerial(RX, TX);
 
 void setup() {
-  // put your setup code here, to run once:
+  modbus_configure(&mySerial, BAUD_RATE, deviceID, RS485_EN, TOTAL_REGS_SIZE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    holdingRegs[0] = 0;
+    holdingRegs[1] = 0;
+    return;
+  }
+
+  holdingRegs[0] = (int)temperature;
+  holdingRegs[1] = (int)humidity;
+  holdingRegs[TOTAL_ERRORS] = modbus_update(holdingRegs);
 }
