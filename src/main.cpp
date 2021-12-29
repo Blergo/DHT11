5fd49a1643549f15b4785d2a21a478a519ef78e6
Blergo <beardedowen@gmail.com>
2021-12-29 18:06:14 +0000

    code for controller to set slave ID

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <SoftwareSerial.h>
 #include <SimpleModbusSlaveSoftwareSerial.h>
 #include <dht.h>
+#include <EEPROM.h>
 
 int DHT11_PIN = 2;
 dht DHT;
@@ -12,7 +13,8 @@ byte humidity = 0;
 #define TX            0
 #define RS485_EN      -1
 #define BAUD_RATE     9600
-#define deviceID      1
+int deviceID = 1;
+int NewID;
 
 unsigned long u32wait = millis() + 500;
 
@@ -33,7 +35,21 @@ SoftwareSerial mySerial(RX, TX);
 void setup() {
   mySerial.begin(BAUD_RATE);
   holdingRegs[1] = 05;
+  EEPROM.begin();
+  EEPROM.get(0, NewID);
+  if (NewID != deviceID){
+    deviceID = NewID;
+  }
   modbus_configure(&mySerial, BAUD_RATE, deviceID, RS485_EN, TOTAL_REGS_SIZE);
+}
+
+void reboot(void) {
+  noInterrupts();
+  CLKPR = 0b10000000;
+  CLKPR = 0;
+  void (*ptrToFunction)();
+  ptrToFunction = 0x0000;
+  (*ptrToFunction)();
 }
 
 void loop() {
@@ -63,6 +79,12 @@ void loop() {
       break;
     }
     u32wait = millis() + 500;
+  }
+
+  if(holdingRegs[0] != 0){
+    EEPROM.put(0, holdingRegs[0]);
+    delay(100);
+    reboot();
   }
   
   holdingRegs[4] = (int)DHT.temperature;
